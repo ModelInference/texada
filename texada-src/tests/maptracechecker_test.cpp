00b4da4739196f943cdaafdce829c4800e86e57a
Caroline Lemieux <caro.lemieux@gmail.com>
2014-07-11 15:26:31 -0700

    More makefile things

--- texada-src/tests/maptracechecker_test.cpp
+++ texada-src/tests/maptracechecker_test.cpp
@@ -20,7 +20,8 @@
 #include <fstream>
 
 
-//
+// Testing map_trace_checker on a small trace, and trying to get decent code coverage
+// for check and find first occ.
 TEST(MapCheckerTest,SmallTrace){
 
     // create the trace map
@@ -44,41 +45,48 @@ TEST(MapCheckerTest,SmallTrace){
 	tpos_vec.push_back(4);
 	trace_map.insert(std::pair<texada::string_event, std::vector<long>>(termvent,tpos_vec));
 
-	// create
+	// create checker and parse error list to parse formula later on
 	texada::map_trace_checker checker = texada::map_trace_checker(&trace_map);
 	spot::ltl::parse_error_list pel;
 
 
-	//Tests to try and cover
+	// Tests to try and cover the "check" functions
 
 	std::string input = "G(a->Fb)";
 	const spot::ltl::formula* f = spot::ltl::parse(input, pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
+	//until
 	input = "a U b";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
+	//weak until
 	input = "a W b";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
+	//release
 	input = "b R a";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_FALSE(checker.check_on_trace(f));
 
+	//strong release
 	input = "b M a";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_FALSE(checker.check_on_trace(f));
 
+	//xor
 	input = "b xor a";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
+	//equiv, iff
 	input = "b <-> a";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_FALSE(checker.check_on_trace(f));
 
+	//impliies
 	input = "a -> b";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_FALSE(checker.check_on_trace(f));
@@ -87,29 +95,37 @@ TEST(MapCheckerTest,SmallTrace){
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
+	//or
 	input = "a | b";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
+	//and
 	input = "a & b";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_FALSE(checker.check_on_trace(f));
 
+	//finally
 	input = "Fa";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
+    //globally
+    input = "Ga";
+    f = spot::ltl::parse(input,pel);
+    ASSERT_FALSE(checker.check_on_trace(f));
+
+	// next
 	input = "Xa";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
+	// not
 	input = "!b";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
 
-
-
-	// tests to try and cover
+	// tests to try and cover finding first occurrence
 
 	// first occ next
 	input = "G(X!a)";
@@ -145,20 +161,38 @@ TEST(MapCheckerTest,SmallTrace){
 	input = "F(F a)";
 	f = spot::ltl::parse(input,pel);
 	ASSERT_TRUE(checker.check_on_trace(f));
+
+	//first occ ->
+	input = "F(a->b)";
+    f = spot::ltl::parse(input,pel);
+    ASSERT_TRUE(checker.check_on_trace(f));
+
+    //first occ xor
+    input = "G(!(a xor b))";
+    f = spot::ltl::parse(input,pel);
+    ASSERT_FALSE(checker.check_on_trace(f));
+
+    //first occ <->
+    input = "F((a<->b))";
+    f = spot::ltl::parse(input,pel);
+    ASSERT_FALSE(checker.check_on_trace(f));
 }
 
+// testing whether map trace mines a more complex formula correctly.
 TEST(MapCheckerTest,ResourceAllocation){
 
-	//parse the ltl formula
+	// parse the ltl formula
 	spot::ltl::parse_error_list pel;
 	const spot::ltl::formula* formula = spot::ltl::parse("(!(b | c) W a) & G((b -> XFc) & (a -> X((!a U c) & (!c U b))) & (c -> X(!(b | c) W a)))", pel);
 
+	// parse log file
 	std::ifstream infile("/home/clemieux/workspace/texada/Texada/traces/resource-allocation/smallabc.txt");
 	texada::simple_parser * parser =  new texada::simple_parser();
 	parser->parse_to_map(infile);
 	std::set<std::map<texada::string_event,std::vector<long>> >  trace_set = parser->return_map_trace();
 	std::set<std::string>  event_set = parser->return_events();
 
+	// check
 	texada::map_trace_checker checker = texada::map_trace_checker(&(*trace_set.begin()));
 	ASSERT_TRUE(checker.check_on_trace(formula));
 }
