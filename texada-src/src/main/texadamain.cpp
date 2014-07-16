4024e02ce89daff2430081f1a56d74686b37b61d
Caroline Lemieux <caro.lemieux@gmail.com>
2014-07-16 12:20:47 -0700

    Cleaned up truncating test

--- texada-src/src/main/texadamain.cpp
+++ texada-src/src/main/texadamain.cpp
@@ -61,45 +61,6 @@ void mine_on_increasing_events(std::string form, bool use_map) {
     }
 }
 
-void set_up_timed_mining_temp_truncator(std::string form, std::string source) {
-    clock_t begin, end;
-    double time_spent;
-    begin = clock();
-    spot::ltl::parse_error_list pel;
-    const spot::ltl::formula* prop_type = spot::ltl::parse(form, pel);
-    texada::simple_parser * parser = new texada::simple_parser();
-    std::ifstream infile(source);
-    parser->parse_to_vector(infile);
-    texada::truncating_checker checker = texada::truncating_checker(prop_type, parser->return_events());
-    checker.return_valid_instants(prop_type, parser->return_vec_trace());
-    delete parser;
-    end = clock();
-    time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
-    std::cout << time_spent << "\n";
-
-}
-
-// timing test for truncator. TODO FIX/REMOVE
-void mine_on_increasing_events_temp_truncator(std::string form){
-    std::string trace_base = std::string(getenv("TEXADA_HOME")) +
-            "/traces/vary-invs-fixed2/log-25000_invs-";
-
-    // we'll mine the property type represented by form on the traces with
-    // the number of unique events in the array below
-    int unique_event_nums[] = //{ 5, 6, 7, 8, 9, 13, 16, 19, 22, 25, 28, 31, 34,
-            //37, 40, 43, 46, 50, 60, 70,
-            {80 };
-    // go through the traces with unique event numbers given from the array
-    // and output time
-    for (int i = 0; i < (sizeof(unique_event_nums) / sizeof(int)); i++) {
-        std::cout << unique_event_nums[i] << " ";
-        set_up_timed_mining_temp_truncator(form, trace_base + std::to_string(unique_event_nums[i] - 1) + ".txt");
-
-    }
-
-}
-
-
 
 /**
  * Main method, runs Texada mining or timing tests depending on options.
@@ -123,8 +84,7 @@ int main(int ac, char* av[]) {
                 "trace file to mine on")("run_on_increasing_events",
                 "run the prop type through traces with increasing number of unique events")(
                 "map_trace,m",
-                "mine on a trace in the form of a map (by default, Texada uses the linear trace checker)")
-                ("run_trunc_test", "Run truncating checker test (TEMPORARY)");
+                "mine on a trace in the form of a map (by default, Texada uses the linear trace checker)");
 
         //parsing the options passed to command line
         boost::program_options::variables_map opts_map;
@@ -161,10 +121,6 @@ int main(int ac, char* av[]) {
         // runs the timing on increasing unique events if specified
         if (opts_map.count("run_on_increasing_events")) {
             mine_on_increasing_events(prop_type, use_map);
-            return 0;
-        }
-        if (opts_map.count("run_trunc_test")) {
-            mine_on_increasing_events_temp_truncator(prop_type);
             return 0;
         }
 
