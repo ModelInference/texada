62659414b8159470fe38a9b8c45b10bc7d5f7e96
Caroline Lemieux <caro.lemieux@gmail.com>
2014-07-16 17:02:03 -0700

    Added response file option, it overrides other options

--- texada-src/src/main/texadamain.cpp
+++ texada-src/src/main/texadamain.cpp
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <string>
 #include <boost/program_options.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/token_functions.hpp>
 #include <ltlvisit/tostring.hh>
 #include <ltlparse/public.hh>
 #include "propertytypeminer.h"
@@ -37,8 +39,8 @@ void set_up_timed_mining(std::string form, std::string source, bool use_map) {
  * @param use_map
  */
 void mine_on_increasing_events(std::string form, bool use_map) {
-    std::string trace_base = std::string(getenv("TEXADA_HOME")) +
-            "/traces/vary-invs-fixed2/log-25000_invs-";
+    std::string trace_base = std::string(getenv("TEXADA_HOME"))
+            + "/traces/vary-invs-fixed2/log-25000_invs-";
 
     // we'll mine the property type represented by form on the traces with
     // the number of unique events in the array below
@@ -59,7 +61,6 @@ void mine_on_increasing_events(std::string form, bool use_map) {
 
     }
 }
-
 
 /**
  * Main method, runs Texada mining or timing tests depending on options.
@@ -83,7 +84,9 @@ int main(int ac, char* av[]) {
                 "log file to mine on")("run_on_increasing_events",
                 "run the prop type through traces with increasing number of unique events")(
                 "map_trace,m",
-                "mine on a trace in the form of a map (by default, Texada uses the linear trace checker)");
+                "mine on a trace in the form of a map (by default, Texada uses the linear trace checker)")(
+                "response_file,r", boost::program_options::value<std::string>(),
+                "specify file containing command line options");
 
         //parsing the options passed to command line
         boost::program_options::variables_map opts_map;
@@ -92,21 +95,50 @@ int main(int ac, char* av[]) {
                 opts_map);
         boost::program_options::notify(opts_map);
 
-        if (opts_map.empty()){
+        if (opts_map.empty()) {
             std::cerr << "Error: no arguments provided. \n";
             std::cout << desc << "\n";
             return 1;
         }
-
-        // if the user wanted to use map, we use map.
-        if (opts_map.count("map_trace"))
-            use_map = true;
 
         // outputs the option information if --help is inputted
         if (opts_map.count("help")) {
             std::cout << desc << "\n";
             return 0;
         }
+
+        // if options are specified in a response file, use it instead.
+        // code mostly borrowed from boost example/response_file.cpp
+        if (opts_map.count("response_file")){
+            std::string input_string = opts_map["response_file"].as<std::string>();
+            std::ifstream infile(input_string);
+            if (!infile) {
+                std::cerr << "Error: Could not open the response file.\n";
+                return 1;
+            }
+            // Read the whole file into a string
+            std::stringstream file_string_stream;
+            file_string_stream << infile.rdbuf();
+            // Split the file content
+            boost::char_separator<char> seperator(" \n\r");
+            std::string file_string = file_string_stream.str();
+            boost::tokenizer<boost::char_separator<char> > tok(file_string,
+                    seperator);
+            std::vector<std::string> args;
+            // copy the options into args
+            std::copy(tok.begin(), tok.end(), back_inserter(args));
+            // Parse the file and store the options
+            boost::program_options::store(
+                    boost::program_options::command_line_parser(args).options(
+                            desc).run(), opts_map);
+
+        }
+
+
+
+        // if the user wanted to use map, we use map.
+        if (opts_map.count("map_trace"))
+            use_map = true;
 
         // places the inputed property type into the prop_type;
         // returns with error if there is none
@@ -139,6 +171,7 @@ int main(int ac, char* av[]) {
             found_instants = texada::mine_map_property_type(prop_type,
                     input_source);
         } else {
+
             found_instants = texada::mine_lin_property_type(prop_type,
                     input_source);
         }
