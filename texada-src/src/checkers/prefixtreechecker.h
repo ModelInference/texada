209ba3e9a5920f8fe56cc66a31c50d4ef5d24400
Caroline Lemieux <caro.lemieux@gmail.com>
2014-07-23 13:32:06 -0700

    Fixed compile issues

--- texada-src/src/checkers/prefixtreechecker.h
+++ texada-src/src/checkers/prefixtreechecker.h
@@ -18,14 +18,14 @@ public:
     prefix_tree_checker();
     virtual ~prefix_tree_checker();
 
-    bool check(const spot::ltl::formula* form_node, prefix_tree_node * trace_node, int id);
+    bool check(const spot::ltl::formula* form_node, shared_ptr<prefix_tree_node> trace_node, int id);
 
 private:
-    bool check(const spot::ltl::atomic_prop* form_node, prefix_tree_node * trace_node, int id);
-    bool check(const spot::ltl::constant* form_node, prefix_tree_node * trace_node, int id);
-    bool check(const spot::ltl::binop* form_node, prefix_tree_node * trace_node, int id);
-    bool check(const spot::ltl::unop* form_node,  prefix_tree_node * trace_node, int id);
-    bool check(const spot::ltl::multop* form_node,  prefix_tree_node * trace_node, int id);
+    bool check(const spot::ltl::atomic_prop* form_node, shared_ptr<prefix_tree_node> trace_node, int id);
+    bool check(const spot::ltl::constant* form_node, shared_ptr<prefix_tree_node> trace_node, int id);
+    bool check(const spot::ltl::binop* form_node, shared_ptr<prefix_tree_node> trace_node, int id);
+    bool check(const spot::ltl::unop* form_node, shared_ptr<prefix_tree_node> trace_node, int id);
+    bool check(const spot::ltl::multop* form_node, shared_ptr<prefix_tree_node> trace_node, int id);
 
     /**
      * both the next types are not supported.
