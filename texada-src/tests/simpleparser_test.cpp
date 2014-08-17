0537f17863ed2ca3fa1c46c8ef4745aaf4f7803a
Dennis Park <emaildpark@gmail.com>
2014-08-17 11:52:34 -0700

    added tests for regex options in simple parser

--- texada-src/tests/simpleparser_test.cpp
+++ texada-src/tests/simpleparser_test.cpp
@@ -183,3 +183,102 @@ TEST(SimpleParserTest, PreTreeTrace) {
 
 
 }
+
+// Checks that the parser can be configured for custom separator line
+TEST(SimpleParserTest, CustomSeparator) {
+    if (getenv("TEXADA_HOME") == NULL){
+        std::cerr << "Error: TEXADA_HOME is undefined. \n";
+        FAIL();
+    }
+
+    std::ifstream infile(
+            std::string(getenv("TEXADA_HOME"))
+                    + "/traces/regex-parsing-tests/custom-separator.txt");
+    texada::simple_parser * parser = new texada::simple_parser;
+    parser->set_separator("break");
+
+    parser->parse_to_vector(infile);
+    std::shared_ptr<std::set<std::vector<texada::string_event>>>trace_set =
+    parser->return_vec_trace();
+
+    delete parser;
+    parser = NULL;
+
+    ASSERT_EQ(trace_set->size(),20)<< "Unexpected number of traces parsed";
+    for (std::set<std::vector<texada::string_event> >::iterator it =
+            trace_set->begin(); it != trace_set->end(); it++) {
+        std::vector<texada::string_event> current_vector = *it;
+        ASSERT_EQ(current_vector.size(), 251);
+    }
+}
+
+// Checks that the parser can be configured to recognize event types using the regex option
+TEST(SimpleParserTest, RegexOption) {
+    if (getenv("TEXADA_HOME") == NULL){
+        std::cerr << "Error: TEXADA_HOME is undefined. \n";
+        FAIL();
+    }
+    std::ifstream infile(
+            std::string(getenv("TEXADA_HOME"))
+                    + "/traces/regex-parsing-tests/small-structured-trace.txt");
+
+    texada::simple_parser * parser = new texada::simple_parser;
+    std::vector<std::string> etypes;
+    etypes.push_back("(?<USER>.*)(?<ETYPE>e[0-9])");
+    parser->set_event_types(etypes);
+
+    parser->parse_to_vector(infile);
+    std::shared_ptr<std::set<std::vector<texada::string_event>>>trace_set =
+    parser->return_vec_trace();
+    std::shared_ptr<std::set<std::string>> events = parser->return_events();
+    delete parser;
+    parser = NULL;
+
+    std::vector<texada::string_event> trace_vec = *trace_set->begin();
+    ASSERT_EQ("e1", trace_vec[0].get_name());
+    ASSERT_EQ("e2", trace_vec[1].get_name());
+    ASSERT_EQ("e3", trace_vec[2].get_name());
+    ASSERT_EQ("e2", trace_vec[3].get_name());
+    ASSERT_TRUE(trace_vec[4].is_terminal());
+
+    ASSERT_EQ(events->size(), 3);
+    ASSERT_TRUE(events->find("e1") != events->end());
+    ASSERT_TRUE(events->find("e2") != events->end());
+    ASSERT_TRUE(events->find("e3") != events->end());
+}
+
+// Checks that the parser can be configured using multiple regular expressions
+TEST(SimpleParserTest, MultipleRegexOptions) {
+    if (getenv("TEXADA_HOME") == NULL){
+        std::cerr << "Error: TEXADA_HOME is undefined. \n";
+        FAIL();
+    }
+    std::ifstream infile(
+            std::string(getenv("TEXADA_HOME"))
+                    + "/traces/regex-parsing-tests/small-structured-trace2.txt");
+
+    texada::simple_parser * parser = new texada::simple_parser;
+    std::vector<std::string> etypes;
+    etypes.push_back("--> (?<ETYPE>e[0-9])");
+    etypes.push_back("<-- (?<ETYPE>e[0-9])");
+    parser->set_event_types(etypes);
+
+    parser->parse_to_vector(infile);
+    std::shared_ptr<std::set<std::vector<texada::string_event>>>trace_set =
+    parser->return_vec_trace();
+    std::shared_ptr<std::set<std::string>> events = parser->return_events();
+    delete parser;
+    parser = NULL;
+
+    std::vector<texada::string_event> trace_vec = *trace_set->begin();
+    ASSERT_EQ("e1", trace_vec[0].get_name());
+    ASSERT_EQ("e2", trace_vec[1].get_name());
+    ASSERT_EQ("e3", trace_vec[2].get_name());
+    ASSERT_EQ("e2", trace_vec[3].get_name());
+    ASSERT_TRUE(trace_vec[4].is_terminal());
+
+    ASSERT_EQ(events->size(), 3);
+    ASSERT_TRUE(events->find("e1") != events->end());
+    ASSERT_TRUE(events->find("e2") != events->end());
+    ASSERT_TRUE(events->find("e3") != events->end());
+}
