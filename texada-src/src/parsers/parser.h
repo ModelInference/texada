c9f61bcca8665a32b4deaf2f1d2230ad4a7647cd
Ivan Beschastnikh <bestchai@cs.ubc.ca>
2014-08-24 09:52:30 -0700

    minor writing nits

--- texada-src/src/parsers/parser.h
+++ texada-src/src/parsers/parser.h
@@ -25,13 +25,13 @@ using std::map;
 using std::shared_ptr;
 
 /**
- * This abstract class implements basic functions of a parser, including
- * methods for configuration and passing in files to be parsed. It also
- * provides a skeletal implementation of the parse function, which calls
- * helper methods to be implemented in derived classes. Concrete parser
- * classes should implement the helper functions end_trace() and add_event()
- * to maintain a data structure of events and a get function for retrieving
- * said data structure.
+ * This abstract class implements basic parsing functions, including
+ * methods for configuring and passing in files to be parsed. It also
+ * provides a skeletal implementation of the parse function, which
+ * calls helper methods that must be implemented in derived
+ * classes. Concrete parser classes should implement the helper
+ * functions end_trace() and add_event() to maintain a data structure
+ * of events and a get function for retrieving this data structure.
  */
 class parser {
 public:
