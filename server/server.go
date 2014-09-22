8f4ac311a93c1831418a411678b4674e6f79a375
Ivan Beschastnikh <bestchai@users.noreply.github.com>
2014-09-21 18:00:54 -0700

    server cmd line args

--- server/server.go
+++ server/server.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"html"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os/exec"
 	"strings"
 	"os"
+	"fmt"
+	"time"
 )
 
 // Compile templates on start.
 var templates = template.Must(template.ParseFiles("static/index.html"))
+
+
+// The input to the webpage template, carries texada output.
+type Output struct {
+    OutputTitle string
+    OutputResult string
+    OutputDisplay string
+}
+
+// Where to find the texada binary
+var texadaCmd string
 
 // Display the template.
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
@@ -20,14 +34,20 @@ func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	}
 }
 
-type Output struct {
-    OutputTitle string
-    OutputResult string
-    OutputDisplay string
+// Outputs an error string.
+func printErrorStr(s string) {
+	fmt.Println(time.Now(), "Error: " + s)
 }
 
-// Handle a mine POST.
+// Outputs the received http request to stdout.
+func printRequest(r *http.Request) {
+	fmt.Println(time.Now(), "REQ:", html.EscapeString(r.URL.Path), ", ip/port: ", r.RemoteAddr)
+}
+
+// Handles POST to /texada/mine/
 func mineHandler(w http.ResponseWriter, r *http.Request) {
+	printRequest(r)
+
 	// Retrieve form data.
 	body := r.FormValue("body")
 	args := r.FormValue("args")
@@ -63,8 +83,7 @@ func mineHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	
-	cmd := "/Users/ivan/repos/texada/texada"
-	outbytes, err := exec.Command(cmd, "-c", argsfile.Name(), logfile.Name()).Output();
+	outbytes, err := exec.Command(texadaCmd, "-c", argsfile.Name(), logfile.Name()).Output();
 	if err != nil {
 		os.Remove(logfile.Name())
 		os.Remove(argsfile.Name())
@@ -77,25 +96,43 @@ func mineHandler(w http.ResponseWriter, r *http.Request) {
 	outstr := strings.TrimSpace(string(outbytes))
 	// cmdfull := cmd + " -c " + argsfile.Name() + " " + logfile.Name()
 
-
 	result := Output{OutputTitle: "Texada output:", OutputResult: outstr, OutputDisplay: "block"}
-	
-	// renderTemplate(w, "index", "Texada output:\n" + outstr)
 	renderTemplate(w, "index", result)
 }
 
+// Handles GET of /texada/
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+	printRequest(r)
+
 	result := Output{OutputTitle: "", OutputResult: "", OutputDisplay: "none"}
 	renderTemplate(w, "index", result)
 }
 
 func main() {
+	// Handle command line args:
+	args := os.Args
+	if len(args) != 3 {
+		printErrorStr("Wrong number of arguments.")
+		fmt.Println("Usage: go run server.go [texada-bin] [port]")
+		fmt.Println("Examples:")
+		fmt.Println("\tgo run server.go /Users/ivan/repos/texada/texada 8080")
+		fmt.Println("\tgo run server.go /home/bestchai/texada/texada 8080")
+		os.Exit(-1)
+	}
+
+	texadaCmd = args[1]
+	port := args[2]
+
+	fmt.Printf("Using texada binary: %s\n", texadaCmd)
+	fmt.Printf("Using port: %s\n", port)
+
+	// Register handlers for different paths.
 	http.HandleFunc("/texada/", viewHandler)
 	http.HandleFunc("/texada/mine/", mineHandler)
 	
-	// Static files handler.
+	// Register static files handler
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	// Listen on port 8080.
-	http.ListenAndServe(":8080", nil)
+	// Listen on port with default IP.
+	http.ListenAndServe(":" + port, nil)
 }
