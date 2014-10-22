467c1bcab723dfd35f6a9206d83f921ce5dc6f27
Dennis Park <emaildpark@gmail.com>
2014-10-21 22:22:49 -0700

    finished implementing support and support potential computing for or statements

--- texada-src/src/checkers/boolbasedchecker.h
+++ texada-src/src/checkers/boolbasedchecker.h
@@ -10,6 +10,7 @@
 
 #include "ltlformulachecker.h"
 #include "statistic.h"
+#include "interval.h"
 
 namespace texada {
 
@@ -171,26 +172,126 @@ protected:
         // for each of the or's children, we check if it is true: if it is,
         // we short circuit and return true. If we have not returned by the
         // end of the loop, then none of the children were true and we return
-        // false. If the produced statistics matter, must check each child.
-
-        // TODO
+        // false.
 
         statistic result = statistic(false, 0, 0);
         statistic result_i;
+        interval intvl(-1, -1);
+        interval intvl_i;
         for (int i = 0; i < numkids; i++) {
             result_i = this->check(node->nth(i), trace_pt);
-            if (result.is_satisfied && result_i.is_satisfied) {
-                result = statistic(true, 0, 0);
-            } else if (!result.is_satisfied && !result_i.is_satisfied) {
-                result = (result.confidence() > result_i.confidence()) ? result : result_i;   // Dennis: not entirely sure about this
-            } else {
-                result = (result_i.is_satisfied) ? result_i : result;
-                if (this->conf_threshold == 1.0 && !this->print_stats && this->sup_threshold == 0 && this->sup_pot_threshold) {
-                    return result;
+            intvl_i = get_interval(node->nth(i));
+            // if the checker is configured to the vanilla setting, short circuit on the first true child
+            if (result_i.is_satisfied &&
+                    this->conf_threshold == 1.0 && !this->print_stats &&
+                    this->sup_threshold == 0 && this->sup_pot_threshold) {
+                return result;
+            }
+
+            // if a child having an interval earlier than another child's interval
+            // is satisfied, the entire property is defined to be vacuously true
+            if (intvl_i < intvl) {
+                if (result_i.is_satisfied) {
+                    return statistic(true, 0, 0);
+                }
+            }
+            // if a child having an interval earlier than another child's interval
+            // is satisfied, the entire property is defined to be vacuously true.
+            // if it is not satisfied, update the result and intvl by the child with
+            // the latest interval
+            else if (intvl < intvl_i) {
+                if (result.is_satisfied) {
+                    return statistic(true, 0, 0);
+                } else {
+                    result = result_i;
+                    intvl = intvl_i;
+                }
+            }
+            // if there exists no child with an interval later all others, evaluate
+            // according to a different scheme taking into account the child with the
+            // greatest confidence and support
+            else {
+                if (result.is_satisfied && result_i.is_satisfied) {
+                    return statistic(true, 0, 0);
+                } else {
+                    if (result.confidence() == result_i.confidence()) {
+                        result = (result.support > result_i.support) ? result : result_i;
+                    } else {
+                        result = (result.confidence() > result_i.confidence()) ? result : result_i;
+                    }
                 }
             }
         }
         return result;
+    }
+
+    /**
+     * returns the interval on which a given formula is potentially evaluated over
+     * @param node the formula to find the interval of
+     * @return
+     */
+    virtual interval get_interval(const spot::ltl::formula * node) {
+        switch (node->kind()) {
+        // interval(true) = interval(false) = interval(p) = [0,0]
+        case spot::ltl::formula::Constant:
+        case spot::ltl::formula::AtomicProp:
+            return interval(0,0);
+
+        case spot::ltl::formula::UnOp: {
+            const spot::ltl::unop* unode = static_cast<const spot::ltl::unop*>(node);
+            switch (unode->op()) {
+            // interval(Gp) = interval(Fp) = [0,inf+]
+            case spot::ltl::unop::G:
+            case spot::ltl::unop::F:
+                return interval(0,LONG_MAX);
+            // interval(Xp) = interval(p) + 1 (i.e. increment both the start and the end of interval(p))
+            case spot::ltl::unop::X:
+                return get_interval(unode->child()) + 1;
+            // interval(Not(p)) = interval(p)
+            case spot::ltl::unop::Not:
+                return get_interval(unode->child());
+            default:
+                std::cerr << "Unsupported unary operator. Returning [-1,-1]. \n";
+                return interval(-1, -1);
+            }
+        }
+
+        case spot::ltl::formula::BinOp: {
+            const spot::ltl::binop* bnode = static_cast<const spot::ltl::binop*>(node);
+            switch (bnode->op()) {
+            // interval(pUq) = interval(pRq) = interval(pWq) = interval(pMq) = [0,inf+]
+            case spot::ltl::binop::U:
+            case spot::ltl::binop::R:
+            case spot::ltl::binop::W:
+            case spot::ltl::binop::M:
+                return interval(0,LONG_MAX);
+            default:
+                std::cerr << "Unsupported binary operator. Returning [-1,-1]. \n";
+                return interval(-1,-1);
+            }
+        }
+
+        case spot::ltl::formula::MultOp: {
+            const spot::ltl::multop* mnode = static_cast<const spot::ltl::multop*>(node);
+            switch (mnode->op()) {
+            // return the
+            case spot::ltl::multop::Or: {
+                return interval(0,0);
+            }
+            case spot::ltl::multop::And: {
+                return interval(0,0);
+            }
+            default:
+                std::cerr << "Unsupported multiple operator. Returning [-1,-1]. \n";
+                return interval(-1,-1);
+            }
+        }
+
+        case spot::ltl::formula::BUnOp:
+        case spot::ltl::formula::AutomatOp:
+        default:
+            return interval(-1,-1);
+        }
     }
 };
 
