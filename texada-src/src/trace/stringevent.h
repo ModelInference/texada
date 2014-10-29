9775b9277f5259174c7de8421782255e01c30ef6
Dennis Park <emaildpark@gmail.com>
2014-10-29 13:57:06 -0700

    began modifying stringevent to be a general event consisting of, optionally, multiple propositions

--- texada-src/src/trace/stringevent.h
+++ texada-src/src/trace/stringevent.h
@@ -9,27 +9,30 @@
 #define STRINGEVENT_H_
 
 #include <string>
+#include <set>
 
 namespace texada {
 
 /**
  * Represents a string event that is parsed from an input log.
  */
-class string_event {
+class string_event {        // to be renamed
 public:
     string_event();
     string_event(std::string);
     virtual ~string_event();
     bool operator<(const string_event&) const;
     bool operator==(const string_event& other) const;
-    std::string get_name() const;
+    std::string get_name() const;       // to be deleted
+    bool is_satisfied(std::string) const;
     bool is_terminal() const;
-    void set_name(std::string);
+    void add_prop(std::string);
+    void set_name(std::string);         // to be deleted
     void set_terminal(bool);
 
 private:
     // The string representation of the event.
-    std::string name;
+    std::set<std::string> props;
 
     // Whether or not this is the last event for a trace.
     bool isTerminal;
