82e665b64833dff37aead11ec3983da58291a176
Dennis Park <emaildpark@gmail.com>
2014-11-01 13:50:46 -0700

    modified mapparser to work with changed parser implementation

--- texada-src/src/parsers/mapparser.cpp
+++ texada-src/src/parsers/mapparser.cpp
@@ -33,31 +33,27 @@ shared_ptr<set<map<string_event, vector<long>>> > map_parser::return_map_trace()
 
 /**
  * Helper function called from within parser::parse(std::ifstream &infile)
- * Ends the current trace being built
- */
-void map_parser::end_trace() {
-    std::vector<long> pos_vec;
-    pos_vec.push_back(pos_count);
-    temp_trace.emplace(string_event(), pos_vec);
-    map_trace_set->insert(temp_trace);
-    temp_trace.clear();
-    pos_count = 0;
-}
-
-/**
- * Helper function called from within parser::parse(std::ifstream &infile)
  * Adds event to the current trace being built
- * @param event name
+ * @param event new event
  */
-void map_parser::add_event(std::string event) {
-    if ((temp_trace.find(string_event(event)) == temp_trace.end())) {
+void map_parser::add_event(string_event event) {
+    if (event.is_terminal()) {
         std::vector<long> pos_vec;
         pos_vec.push_back(pos_count);
-        temp_trace.emplace(string_event(event), pos_vec);
-        pos_count++;
+        temp_trace.emplace(event, pos_vec);
+        map_trace_set->insert(temp_trace);
+        temp_trace.clear();
+        pos_count = 0;
     } else {
-        temp_trace.at(string_event(event)).push_back(pos_count);
-        pos_count++;
+        if ((temp_trace.find(event) == temp_trace.end())) {
+            std::vector<long> pos_vec;
+            pos_vec.push_back(pos_count);
+            temp_trace.emplace(event, pos_vec);
+            pos_count++;
+        } else {
+            temp_trace.at(event).push_back(pos_count);
+            pos_count++;
+        }
     }
 }
 
