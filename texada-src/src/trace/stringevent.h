ce3c1c57670f07c797aab8d497cdfb2f2270eae2
Dennis Park <emaildpark@gmail.com>
2014-11-01 16:09:18 -0700

    completed modifying parser to use general, optionally multi-propositional, event in place of string-represented event.

--- texada-src/src/trace/stringevent.h
+++ texada-src/src/trace/stringevent.h
@@ -30,6 +30,7 @@ public:
     void add_prop(std::string);
     void set_name(std::string);         // to be deleted
     void set_terminal(bool);
+    void clear();
 
 private:
     // The string representation of the event.
