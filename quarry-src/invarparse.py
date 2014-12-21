79715d3da27e2337e2e05b1e9925d088c77fd897
Dennis Park <emaildpark@gmail.com>
2014-12-20 20:54:46 -0800

    implemented invariant parser

--- quarry-src/invarparse.py
+++ quarry-src/invarparse.py
@@ -7,13 +7,20 @@ class InvariantParser:
 
    def __init__(self, filepath):
       '''Return a InvariantParser object which is set to parse the file located at *filepath*.'''
-      # TODO
+      self.filepath = filepath
+      self.set_separator("===========================================================================")
 
    def parse_invariants(self):
       '''Return a dictionary mapping ppts to sets of invariants parsed from the parser's initialized invariants file.'''
-      # TODO
+      rtn_dict = {}
+      with open(self.filepath) as f:
+         contents = f.read()
+         for entry in contents.split(self.separator_str):
+            entry = entry.strip().split("\n")
+            rtn_dict[entry[0]] = entry[1:]
+      return rtn_dict
 
    def set_separator(self, regex):
       '''Set the parser's separator regex. The separator regex is used to partition the invariants 
       file into a list of invariant records.'''
-      # TODO
+      self.separator_str = regex
