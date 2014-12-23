0722577e0ffa20c1a5bdf2c97ebf435e7bc34f15
Dennis Park <emaildpark@gmail.com>
2014-12-22 16:46:44 -0800

    implemented dtrace parser

--- quarry-src/dtraceparse.py
+++ quarry-src/dtraceparse.py
@@ -1,44 +1,68 @@
+import collections, sys, re
+from itertools import izip
+
 class DTraceParser:
    '''The DTraceParser is used to parse a Daikon-generated dtrace file into a collection of event traces.
    More specifically, the parser returns a dictionary mapping each object occuring in the dtrace file 
    with its sequence of method entries and exits.'''
 
    filepath = ""
-   event_regex = ""
-   separator_regex = ""
-   is_slurping = True
+   separator_str = ""
 
-   def __init__(self,filepath):
+   def __init__(self, filepath):
       '''Return a DTraceParser object which is set to parse the file located at *filepath*.'''
-      # TODO
+      self.filepath = filepath
+      self.set_separator("(\n){2,}")
 
-   def parse_dtrace():
-      '''Return a dictionary of object event traces parsed from the parser's initialized dtrace file.'''
-      # TODO
+   def parse_dtrace(self):
+      '''Return a dictionary of event traces (one for each object appearing in the dtrace) parsed from the initialized dtrace file.'''
+      rtn_dict = collections.defaultdict(list)
+      records = self.get_records()
+      for i, rec in enumerate(records):
+         obj_id = ""
+         if "this" in rec.data:
+            obj_id = rec.data["this"]
+         # if rec is a constructor entry (i.e. method name matches class name) find the object id from its corresponding exit point
+         # elif rec.class_name.endswith(rec.method_name.rsplit('(')[0]):
+         elif rec.method_name[0].isupper():
+            for rec_j in records[i:]:
+               if rec.data["this_invocation_nonce"] == rec_j.data["this_invocation_nonce"] and "this" in rec_j.data:
+                  obj_id = rec_j.data["this"]
+                  break
+         else:
+            # TODO: if rec is a non-constructor static method?
+            continue
+         rtn_dict[rec.class_name+obj_id].append(rec[:4])
+      return rtn_dict
 
-   def set_lazy():
-      '''Set the parser to lazy read the dtrace file. If lazy reading is turned on, the parser will 
-      read the file chunk by chunk. By default, the parser is set to non-lazy reading, and the dtrace
-      file will be fully loaded to memory before being processed. It is recommended that lazy reading
-      be turned if the dtrace file is very large.'''
-      # TODO
-
-   def is_lazy():
-      '''Returns whether the parser is configured to lazy read.'''
-      # TODO
-
-   def set_separator(regex):
+   def set_separator(self, regex):
       '''Set the parser's separator regex. The separator regex is used to partition the dtrace file into a list of ppt records.'''
-      # TODO
+      self.separator_str = regex
 
-   def set_event_classes(regex):
-      '''Set the parser's event regex. The event regex is used to determine the class of a given program point. By default, the class of a program point refers to the corresponding object.'''
-      # TODO
+   def get_records(self):
+      '''Returns a list of ppt records extracted from the initialized dtrace file'''
+      # a struct representing a ppt record
+      PPTRecord = collections.namedtuple("PPTRecord", ["class_name", "method_name", "ppt_type", "ppt_id", "data"])
+      rtn_list = []
+      with open(self.filepath) as f:
+         contents = f.read()
+         records = re.split(self.separator_str, contents)
+         regex = re.compile("(?P<class_name>^(\w+\.)*\w+)\.(?P<method_name>\w+)\(.*?\):::(?P<ppt_type>ENTER|EXIT)(?P<ppt_id>[0-9]*)")
+         for record in records:
+            record = record.split("\n")
+            match = regex.match(record[0])
+            # if did not match regex, was not a valid ppt record; ignore and continue iteration
+            if match == None:
+               continue
+            else:
+               i = iter(record[1:])
+               data = dict(izip(i, i))
+               rtn_list.append(PPTRecord(match.group("class_name"),
+                                         match.group("method_name"),
+                                         match.group("ppt_type"),
+                                         match.group("ppt_id"),
+                                         data))
+      return rtn_list
 
-   def extract_event(datablock):
-      '''Returns an (event,event_class) pair from a ppt record.'''
-      # TODO
 
-   def get_list_of_records(filepath):
-      '''Returns a list of ppt records partitioned from a dtrace file.'''
-      # TODO
+
--- quarry-src/quarry.py
+++ quarry-src/quarry.py
@@ -1,11 +1,11 @@
-import sys, re
-from argparse import ArgumentParser
-#from invarparse import InvariantParser
-#from dtraceparse import DTraceParser
+import re
+from argparse     import ArgumentParser
+from invarparse   import InvariantParser
+from dtraceparse  import DTraceParser
 
 def main():
    ''' Returns a data-temporal log generated from combining a pair of Daikon-generated dtrace and invariants files.
-   Usage: quarry.py [-h] [-o] [-c] [-l] [-e EXTRACT] <dtracefile> <invarfile> <outputfile>'''
+   Usage: quarry.py [-h] [-o] [-c] [-e EXTRACT] <dtracefile> <invarfile> <outputfile>'''
 
    # process options
    argparser = ArgumentParser()
@@ -14,14 +14,11 @@ def main():
    argparser.add_argument("ofile", help="path to the output file.")
    argparser.add_argument("--incl_obj",         help="include object invariances",  action="store_true")
    argparser.add_argument("--incl_cls",         help="include class invariances",   action="store_true")
-   argparser.add_argument("-l", "--lazy_read",  help="lazy read dtrace file [recommended if file is too big]", action="store_true")
    argparser.add_argument("-e", "--extract",    help="specify a specific class to extract [by default: extract all]")
    args = argparser.parse_args()
 
    # parse dtrace file
    dtraceparser = DTraceParser(args.dfile)
-   if args.lazy_read:
-      dtraceparser.set_lazy()
    if args.extract:
       # TODO: configure dtraceparser to extract only the specified class
    traces = parse_dtrace()
