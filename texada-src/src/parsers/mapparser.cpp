6f74bc56cc04952405616a87707ffc322c441ba2
Caroline Lemieux <caro.lemieux@gmail.com>
2015-05-13 13:53:12 -0700

    Modified map parser to fix Issue57

--- texada-src/src/parsers/mapparser.cpp
+++ texada-src/src/parsers/mapparser.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "mapparser.h"
+#include "../trace/event.h"
 
 namespace texada {
 
@@ -36,26 +37,32 @@ shared_ptr<set<map<event, vector<long>>> > map_parser::return_map_trace() {
  * Adds event to the current trace being built
  * @param event new event
  */
-void map_parser::add_event(event event) {
-    parser::add_event(event);
-    if (event.is_terminal()) {
+void map_parser::add_event(event event_parent) {
+    // if event_parent is terminal, add and return
+    if (event_parent.is_terminal()) {
         std::vector<long> pos_vec;
         pos_vec.push_back(pos_count);
-        temp_trace.emplace(event, pos_vec);
+        temp_trace.emplace(event_parent, pos_vec);
         map_trace_set->insert(temp_trace);
         temp_trace.clear();
         pos_count = 0;
-    } else {
-        if ((temp_trace.find(event) == temp_trace.end())) {
-            std::vector<long> pos_vec;
-            pos_vec.push_back(pos_count);
-            temp_trace.emplace(event, pos_vec);
-            pos_count++;
+        return;
+    }
+    // else add each atomic proposition to an individual map
+    std::set<std::string> set_of_event_names = event_parent.get_props();
+    for (std::set<std::string>::iterator it = set_of_event_names.begin(); it != set_of_event_names.end(); it++){
+        event event_ = event(*it);
+        parser::add_event(event_);
+        if ((temp_trace.find(event_) == temp_trace.end())) {
+          std::vector<long> pos_vec;
+          pos_vec.push_back(pos_count);
+          temp_trace.emplace(event_, pos_vec);
         } else {
-            temp_trace.at(event).push_back(pos_count);
-            pos_count++;
+          temp_trace.at(event_).push_back(pos_count);
         }
     }
+   // increment position only after all atomic props have been added
+   pos_count++;
 }
 
 } /* namespace texada */
