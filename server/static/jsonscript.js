e40d900aec564936600260c1ac5d41cb8774fb2d
Caroline Lemieux <caro.lemieux@gmail.com>
2015-08-17 12:02:57 -0700

    Added hidden output before mining

--- server/static/jsonscript.js
+++ server/static/jsonscript.js
@@ -1,5 +1,7 @@
  $( document ).ready(function() {
          var out_obj = {}
+         var output_loaded = false
+         var all_out = '<td id = "allout" valign="top"> <h3> Output: </h3> <div id="result" class="output" style="display:block"> <table> <tr><td><b> Bindings <b></td><td> <div class="onoffswitch"> <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch"> <label class="onoffswitch-label" for="myonoffswitch"> <span class="onoffswitch-inner"></span> <span class="onoffswitch-switch"></span> </label> </div></td><td> <b> Property Instantiations </b></td></tr> </table><table> <tr id="realout"> <td> </td></tr></table> </div> </td>'
          var bindings_str = ""
          var instants_str = ""
          $("#form1").submit(function(event) {
@@ -26,20 +28,27 @@
               }
 
               if (instants.length == 0){
-                  bindings_str = "none"
-                  instants_str = "none"
-              }
+                  output_loaded = false
+                  $("#allout").replaceWith('<td id = "allout" valign="top"><h3>Output:</h3><div id="result" class="output" style="display:block"> No valid instantiations. </div> </td>')
+              } else{
+
               // decide which format to output
               if ($("#myonoffswitch").is(":checked")){
+                if (!output_loaded)
+                   $("#allout").replaceWith(all_out)
                 $("#realout").replaceWith('<tr id="realout"> <td>' + instants_str + '</td></tr>')
+                output_loaded = true
               } else {
+                 if (!output_loaded)
+                    $("#allout").replaceWith(all_out)
                  $("#realout").replaceWith('<tr id="realout"> <td>' + bindings_str + '</td></tr>')
-              }
+                 output_loaded = true
+              }}
             });
          });
 
         // switch output format when switch button is clicked
-         $("#myonoffswitch").click(function() {
+         $('body').on('click','#myonoffswitch', function() {
               if ($("#myonoffswitch").is(":checked")){
                 $("#realout").replaceWith('<tr id="realout"> <td>' + instants_str + '</td></tr>')
               } else {
@@ -68,3 +77,11 @@ function instantiateString(var_posns, var_binds, str) {
    return str
    
 }
+
+
+
+
+
+
+
+
