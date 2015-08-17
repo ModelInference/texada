c681768cbee235fbd20926abf136c47c7b8fab5a
Caroline Lemieux <caro.lemieux@gmail.com>
2015-08-17 16:34:08 -0700

    Added support/support pot/confidence printing: had to expand output box

--- server/static/jsonscript.js
+++ server/static/jsonscript.js
@@ -1,7 +1,7 @@
  $( document ).ready(function() {
          var out_obj = {}
          var output_loaded = false
-         var all_out = '<td id = "allout" valign="top"> <h3> Output: </h3> <div id="result" class="output" style="display:block"> <table> <tr><td><b> Bindings <b></td><td> <div class="onoffswitch"> <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch"> <label class="onoffswitch-label" for="myonoffswitch"> <span class="onoffswitch-inner"></span> <span class="onoffswitch-switch"></span> </label> </div></td><td> <b> Property Instantiations </b></td></tr> </table><table> <tr id="realout"> <td> </td></tr></table> </div> </td>'
+         var all_out = '<td id = "allout" valign="top"> <h3> Output: </h3> <div id="result" class="output" style="display:block"> <table> <tr><td><b> Bindings <b></td><td> <div class="onoffswitch"> <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch"> <label class="onoffswitch-label" for="myonoffswitch"> <span class="onoffswitch-inner"></span> <span class="onoffswitch-switch"></span> </label> </div></td><td> <b> Property Instantiations </b></td></tr> </table><table id="realout"> <tr > <td> </td></tr></table> </div> </td>'
          var bindings_str = ""
          var instants_str = ""
          $("#form1").submit(function(event) {
@@ -12,9 +12,18 @@
             $.post("/texada/mine/", in_str, function( data ) {
               out_obj = jQuery.parseJSON(data)
               var instants = out_obj["prop-instances"]
+              var stats = false
               bindings_str = "";
               instants_str = "";
+              if (instants.length != 0)
+                 var stats = instants[0].hasOwnProperty("stats")
+              if (stats){
+                  bindings_str = "<tr> <td>Binding</td> <td>Support</td> <td>Support Potential</td> <td>Confidence</td></tr>"
+                  instants_str = "<tr> <td>Instance</td> <td>Support</td> <td>Support Potential</td> <td>Confidence</td></tr>"
+              }
               for (var i = 0; i < instants.length; i++){ 
+                  bindings_str += "<tr> <td>"
+                  instants_str += "<tr> <td>"
                   var vars = instants[i]["vars"]
                   var l = i + 1
                   bindings_str += l + ". ["
@@ -24,8 +33,15 @@
                   }
                   // remove last comma
                   bindings_str = bindings_str.slice(0,-2)
-                  bindings_str +=  "]<br>"
-                 instants_str += instantiateString(out_obj["prop-type"]["vars"], vars, out_obj["prop-type"]["str"])+ "<br>"
+                  bindings_str +=  "]</td>"
+                 instants_str += instantiateString(out_obj["prop-type"]["vars"], vars, out_obj["prop-type"]["str"])+ "</td>"
+                 if (instants[i].hasOwnProperty("stats")){
+                       var stat_str = '<td align= "center">'+ instants[i]["stats"]["support"] +"</td>" + '<td align= "center">'+ instants[i]["stats"]["support potential"] +"</td>" + '<td align= "center">'+ instants[i]["stats"]["confidence"] +"</td>"
+                       bindings_str += stat_str
+                       instants_str += stat_str
+                  }
+                  bindings_str += "</tr>"
+                  instants_str += "</tr>"
               }
 
               if (instants.length == 0){
@@ -37,12 +53,12 @@
               if ($("#myonoffswitch").is(":checked")){
                 if (!output_loaded)
                    $("#allout").replaceWith(all_out)
-                $("#realout").replaceWith('<tr id="realout"> <td>' + instants_str + '</td></tr>')
+                $("#realout").replaceWith('<table id="realout">' + instants_str + '</table>')
                 output_loaded = true
               } else {
                  if (!output_loaded)
                     $("#allout").replaceWith(all_out)
-                 $("#realout").replaceWith('<tr id="realout"> <td>' + bindings_str + '</td></tr>')
+                 $("#realout").replaceWith('<table id="realout">' + bindings_str + '</table>')
                  output_loaded = true
               }}
             });
@@ -51,9 +67,9 @@
         // switch output format when switch button is clicked
          $('body').on('click','#myonoffswitch', function() {
               if ($("#myonoffswitch").is(":checked")){
-                $("#realout").replaceWith('<tr id="realout"> <td>' + instants_str + '</td></tr>')
+                $("#realout").replaceWith('<table id="realout">' + instants_str + '</table>')
               } else {
-                 $("#realout").replaceWith('<tr id="realout"> <td>' + bindings_str + '</td></tr>')
+                 $("#realout").replaceWith('<table id="realout">' + bindings_str + '</table>')
               }
          });
 
