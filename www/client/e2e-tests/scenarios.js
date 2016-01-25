4b64a904a99c75eece6364b26827f9afed0fb598
Shehzad Noor Taus Priyo <11179115+sntaus@users.noreply.github.com>
2016-01-24 20:31:56 -0800

    * Added more e2e tests

--- www/client/e2e-tests/scenarios.js
+++ www/client/e2e-tests/scenarios.js
@@ -1,13 +1,36 @@
 'use strict';
 
-/* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
 describe('Texada', function () {
-
-
-    it('should automatically redirect to /view1 when location hash/fragment is empty', function () {
+    beforeEach(function () {
         browser.get('');
+    });
+
+    it('should have title "Texada"', function () {
         expect(browser.getTitle()).toBe("Texada");
+    });
+
+    it('should have splash screen video loaded', function () {
+        expect($(".youtubevideo").getAttribute("src")).toBe("https://www.youtube.com/embed/sNeGZG1LN9U");
+    });
+
+    it('should have default args', function () {
+        expect($("#argumentsText").getAttribute("value")).toBe("-f 'G(x->XFy)' -l");
+    });
+
+    it('should have default log', function () {
+        expect($("#inputText").getAttribute("value")).not.toBeNull();
+        expect($("#inputText").getAttribute("value")).not.toBe("");
+    });
+
+    it('should submit form if mine clicked', function () {
+        $("#inputBtn").click().then(function () {
+            $("#mineButton").click().then(function () {
+                expect($("#outputTab").isDisplayed()).toBeTruthy();
+            });
+        });
+
+
     });
 
 
