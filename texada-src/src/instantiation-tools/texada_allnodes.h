4ea4d5e958627c2caa7607339a3683a9de857d83
Christopher Banks <christopherjbanks1@gmail.com>
2022-01-16 16:14:51 -0500

    adds header files for common automata

---
+++ texada-src/src/instantiation-tools/texada_allnodes.h
@@ -0,0 +1,15 @@
+#ifndef TEXADA_ALLNODES_H
+#define TEXADA_ALLNODES_H
+/*
+ * texada_allnodes.h
+ * Created on: December 20, 2021
+ * Author: cjbanks
+ */
+
+#include "texada_atomicprop.h"
+#include "texada_unop.h"
+#include "texada_binop.h"
+#include "texada_multop.h"
+
+
+#endif //TEXADA_ALLNODES_H
