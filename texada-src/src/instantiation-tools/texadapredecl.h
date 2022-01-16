a1f02f04e883468a84c4241db19e8eb6ae48558e
Christopher Banks <christopherjbanks1@gmail.com>
2022-01-16 16:36:18 -0500

    adds pre-declaration of SPOT formula objects

---
+++ texada-src/src/instantiation-tools/texadapredecl.h
@@ -0,0 +1,23 @@
+/*
+ * texadapredecl.h
+ * Created On: June 24, 2021
+ * Author: cjbanks
+ */
+ 
+#ifndef TEXADAPREDECL_H_
+#define TEXADAPREDECL_H_
+
+namespace texada {
+	namespace ltl{
+		    struct visitor;
+		    class formula;
+		    class atomic_prop;
+		    class constant;
+		    class binop;
+		    class unop;
+		    class multop;
+		    class automatop;
+		    class bunop;
+	} 
+}
+#endif /*TEXADAPREDECL_H_*/
